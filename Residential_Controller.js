6f757fd32dc8fa66fe9d8c8bccbacb2a770c6fd6
alexgreyer18 <60399333+alexgreyer18@users.noreply.github.com>
2020-02-18 22:56:17 -0500

    Feb 18 23h00 --- push to requestList working

--- Residential_Controller.js
+++ Residential_Controller.js
@@ -5,68 +5,125 @@ Méthode 2: RequestFloor(Elevator, RequestedFloor)
 La méthode 2 doit obligatoirement faire bouger les ascenseurs dans son traitement. 
 */
 
-var selectedElevator = null;
-var waitList = [];
-var floorList = [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 ];
-var RC = floor.indexOf('0');
+class Column {
+	constructor(nbFloor, nbElevator) {
+		this.floorList = [];
+		for (let i = 1; i <= nbFloor; i++) {
+			this.floorList.push([ i ]);
+		}
+
+		this.elevatorList = [];
+		for (let i = 0; i < nbElevator; i++) {
+			this.elevatorList.push(new Elevator('elevator' + i, 10));
+		}
+
+		this.buttonList = [];
+		for (let i = 1; i <= nbFloor; i++) {
+			if (i != 1) {
+				var callButton = new Button('down', i);
+				this.buttonList.push(callButton);
+			}
+			if (i != nbFloor) {
+				var callButton = new Button('up', i);
+				this.buttonList.push(callButton);
+			}
+		}
+	}
+
+	findElevator(requestedFloor, requestDirection) {
+		column.elevatorList.forEach((elevator) => {
+			if (elevator.currentDirection == 'idle') {
+				console.log(elevator.elevatorId + ' is Idle');
+				return elevator;
+			}
+
+			if (requestedFloor === elevator.currentFloor) {
+				//The elevator is in the same floor of the client
+				console.log('Same floor');
+				return elevator;
+			} else {
+				//The elevator is in a diferent floor from the client
+
+				if (requestedFloor > elevator.currentFloor) {
+					//To check if the client is above floor the elevator
+					console.log('Client above');
+
+					if (
+						requestDirection === 'down' &&
+						elevator.currentDirection === 'down'
+					) {
+						//This elevator is above the client and going down so its a match
+						console.log('Direction match down');
+						return elevator;
+					}
+				} else {
+					//The client is below the elevator
+					console.log('Client below');
+
+					if (
+						requestDirection === 'up' &&
+						elevator.currentDirection === 'up'
+					) {
+						//This elevator is below the client and going up so its a match
+						console.log('Direction match up');
+						return elevator;
+					}
+				}
+			}
+		});
+
+		let BestElevator = null;
+		column.elevatorList.forEach((elevator) => {
+			if (BestElevator == null) {
+				//If the comparison point has not been set yet
+				BestElevator = elevator;
+			} else {
+				if (
+					BestElevator.requestList.length >
+					elevator.requestList.length
+				) {
+					BestElevator = elevator;
+				}
+			}
+		});
+		return BestElevator;
+	}
+
+	requestElevator(requestedFloor, direction) {
+		let bestElevator = column.findElevator(requestedFloor, direction);
+		bestElevator.requestList.push(requestedFloor);
+		bestElevator.requestList.sort();
+		bestElevator.currentDirection = direction;
+	}
+}
 
 // Elevator constructor
-function Elevator(currentFloor, currentDirection, currentStatus) {
-	this.currentFloor = currentFloor;
-	this.currentDirection = currentDirection;
-	this.currentStatus = currentStatus;
-}
-
-let currentFloor = 3;
-let currentDirection = 'up';
-let currentStatus = 'idle';
-
-function makeElevators(n) {
-	let elevatorList = new Array(n);
-	for (var i = 0; i < n; ++i) {
-		elevatorList[i] = new Elevator(
-			currentFloor,
-			currentDirection,
-			currentStatus
-		);
+class Elevator {
+	constructor(elevatorId, nbFloor) {
+		this.elevatorId = elevatorId;
+		this.floorRequestButtons = [];
+		for (let i = 1; i <= nbFloor; i++) {
+			this.floorRequestButtons.push(i);
+		}
+		this.currentFloor = 1;
+		this.currentDirection = 'idle';
+		this.requestList = [];
+		this.doors = 'closed';
 	}
-	return elevatorList;
 }
 
-var elevatorList = makeElevators(2);
-
-console.log(elevatorList);
-
-function ButtonUp(floor, direction) {
-	this.floor = floor;
-	this.direction = direction;
-}
-
-function makeButtonUps(n) {
-	let buttonUpList = new Array(n);
-	for (var i = 0; i < n; ++i) {
-		buttonUpList[i] = new ButtonUp(floor, direction);
+// Button constructor
+class Button {
+	constructor(requestedDir, floor) {
+		this.floor = floor;
+		this.requestedDir = requestedDir;
+		this.lightStatus = 'Off';
 	}
-	return buttonUpList;
 }
 
-var buttonUpList = makeButtonUps(10);
-
-console.log(buttonUpList);
-
-function ButtonDown(floor, direction) {
-	this.floor = floor;
-	this.direction = direction;
-}
-
-function makeButtonDowns(n) {
-	let buttonDownList = new Array(n);
-	for (var i = 0; i < n; ++i) {
-		buttonDownList[i] = new ButtonDown(floor, direction);
-	}
-	return buttonDownList;
-}
-
-var buttonDownList = makeButtonDowns(10);
-
-console.log(buttonDownList);
+//SECTION TESTS
+var column = new Column(10, 2);
+// var bestElevator = column.findElevator();
+//bestElevator.move();
+column.requestElevator(3, 'down');
+console.log(column.elevatorList);
