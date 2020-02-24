36174d8e30b5c47ff4de98e2760f2cd9e55c4360
alexgreyer18 <60399333+alexgreyer18@users.noreply.github.com>
2020-02-24 18:26:30 -0500

    Feb 24 18h30

--- Commercial_Controller.cs/Program.cs
+++ Commercial_Controller.cs/Program.cs
@@ -1,73 +1,88 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Commercial_Controller.cs
 {
-    // public class Battery
-    // {
-    //     columnList
-    //     for (int i = 0; i < nbColumn; i++)
-    //     {
-            
-    //     }
-       
-        
+  public class Columns
+  {
+    
+  }
+  public class Elevator
+  {
+    private string elevatorId;
+    private Int32 currentFloor = 1;
+    private string currentDirection = "idle";
 
-    //     public string CommandPanel()
-    //     {
-    //         string selectedColumn = bestColumn;
-    //         return selectedColumn;
-    //     }
-        
-    // }
-
-    // public class Column
-    // {
-        
-    //     Int32[] elevatorList = new Array[nbOfElevators];
-
-	// 	for (let i = 0; i < nbElevator.Length; i++) 
-    //     {
-	// 		this.elevatorList.push(new Elevator('elevator' + i, 10));
-	// 	}
-
-
-    //     buttonList
-
-    // }
-
-    public class Elevator
+    public Elevator(string elevatorId)
     {
-        public string elevatorId;
-        public Int32 nbFloor;
-        public Int32 currentFloor = 1;
-        public string currentDirection = "idle";
-        public string doors = "Elevator doors are closed";
-
-
-        public Elevator(string aElevatorId, Int32 aNbFloor)
-        {
-            elevatorId = aElevatorId;
-            nbFloor = aNbFloor;
-        }
+      this.elevatorId = elevatorId;
+      this.currentFloor = currentFloor;
+      this.currentDirection = currentDirection;
     }
 
-    class Program
+    public string ElevatorId
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-
-            Elevator f1 = new Elevator("elevator1", 66);
-            Console.WriteLine(f1.nbFloor);
-
-            Int32[] floorList = {9, 8, 7};
-            for (int i = 0; i < floorList.Length; i++)
-		    {
-			    Console.WriteLine(floorList[i]);
-		    }
-        
-            // var battery = new Battery(66, 4);
-            
-        }
+      get { return elevatorId;  }
+      set { elevatorId = value; }
     }
+
+    public int CurrentFloor
+    {
+      get { return currentFloor; }
+      set { currentFloor = value; }
+    }
+    public string CurrentDirection
+    {
+      get { return currentDirection; }
+      set { currentDirection = value; }
+    }
+  }
+
+  class Program
+  {
+      static void Main(string[] args)
+      {
+        Console.WriteLine("Hello World!");
+
+        Int32[] floorList = {9, 8, 7};
+        for (int i = 0; i < floorList.Length; i++)
+        {
+          Console.WriteLine(floorList[i]);
+        }
+
+
+        List<Elevator> ElevatorList = new List<Elevator>();
+
+        int numColumns = 4;
+        int numElevators = (numColumns * 5) + 1;
+
+        for(int i = 1; i < numElevators; i++)
+        {
+          if(i <= 5)
+          {
+            ElevatorList.Add(new Elevator("Basement-Elevator" + i));
+          } else if (i > 5 & i <=10)
+          {
+            ElevatorList.Add(new Elevator("Col2-Elevator" + i));
+          } else if (i > 10 & i <=15)
+          {
+            ElevatorList.Add(new Elevator("Col3-Elevator" + i));
+          } else if (i > 15 & i <=20)
+          {
+            ElevatorList.Add(new Elevator("Col4-Elevator" + i));
+          }
+        }
+        
+
+        foreach (var elevator in ElevatorList)
+        {
+          Console.WriteLine("Elevator: {0},{1},{2}", elevator.ElevatorId, elevator.CurrentFloor, elevator.CurrentDirection);
+        }
+        
+        int[] nums = new int[10];
+        Console.WriteLine(nums.Length);
+          // var battery = new Battery(66, 4);
+          
+      }
+  }
 }
